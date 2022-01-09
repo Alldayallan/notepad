244effcdae7e611bb43ba3afc96569e1ee7f2b33
Allan <mail@allanreid.co>
2022-01-09 11:39:45 -0800

    main layout complete

--- notepad.py
+++ notepad.py
@@ -1,5 +1,77 @@
 #Notepad app
 
 import tkinter
+from tkinter import font
+from typing_extensions import IntVar
+from PIL import ImageTk, Image
+from tkinter import StringVar, IntVar
 
 #Define window
+root = tkinter.Tk()
+root.title('Notepad')
+root.iconbitmap('pad.ico')
+root.geometry('600x600')
+root.resizable(0,0),
+
+#Define fonts and colors
+text_color = "#fffacd"
+menu_color = "#dbd9db"
+root_color = "#6c809a"
+root.config(bg=root_color)
+
+
+#Define functions
+#Define frames
+menu_frame = tkinter.Frame(root, bg=menu_color)
+text_frame = tkinter.Frame(root, bg=text_color)
+menu_frame.pack(padx=5, pady=5)
+text_frame.pack(padx=5, pady=5)
+
+#Layout for menu frame
+#Create the menu: new, open, save, close, font family, font size, font option
+new_image = ImageTk.PhotoImage(Image.open('new.png'))
+new_button = tkinter.Button(menu_frame, image=new_image)
+new_button.grid(row=0, column=0, padx=5, pady=5)
+
+open_image = ImageTk.PhotoImage(Image.open('open.png'))
+open_button = tkinter.Button(menu_frame, image=open_image)
+open_button.grid(row=0, column=1, padx=5, pady=5)
+
+save_image = ImageTk.PhotoImage(Image.open('save.png'))
+save_button = tkinter.Button(menu_frame, image=save_image)
+save_button.grid(row=0, column=2, padx=5, pady=5)
+
+close_image = ImageTk.PhotoImage(Image.open('close.png'))
+close_button = tkinter.Button(menu_frame, image=close_image, command=root.destroy)
+close_button.grid(row=0, column=3, padx=5, pady=5)
+
+#Create a list of fonts to use
+families = ['Terminal', 'Modern', 'Script', 'Courier', 'Arial', 'Calibri', 'Cambria', 'Georgia', 'MS Gothic', 'Comic Sans MS', 'SimSun', 'Tahoma', 'Times New Roman', 'Verdana', 'Wingdings', 'Ubuntu', 'Roboto']
+font_family = StringVar()
+font_family_drop = tkinter.OptionMenu(menu_frame, font_family, *families)
+font_family.set('Terminal')
+#Set the widtch so it will fit "times new roman" and remain constant
+font_family_drop.config(width=16)
+font_family_drop.grid(row=0, column=4, padx=5, pady=5)
+
+sizes = [8, 10, 12, 14, 16, 18, 20, 24, 32, 48, 64, 72, 96]
+font_size = IntVar()
+font_size_drop = tkinter.OptionMenu(menu_frame, font_size, *sizes)
+font_size.set(12)
+#Set width to be constant even if it 8.
+font_size_drop.config(width=2)
+font_size_drop.grid(row=0, column=5, padx=5, pady=5)
+
+options = ['None', 'Bold', 'Italic']
+font_option = StringVar()
+option_drop = tkinter.OptionMenu(menu_frame, font_option, *options)
+font_option.set('none')
+option_drop.config(width=5)
+option_drop.grid(row=0, column=6, padx=5, pady=5)
+
+
+
+
+
+#Run the root window's main loop
+root.mainloop()
